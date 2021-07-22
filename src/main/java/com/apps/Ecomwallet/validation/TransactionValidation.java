aa7c3a7ab59f52595524ea2507af1f117ec0c9cd
TieBoon <ngtieboon@gmail.com>
2021-07-22 14:32:24 +0800

    Commit balance check

--- src/main/java/com/apps/Ecomwallet/validation/TransactionValidation.java
+++ src/main/java/com/apps/Ecomwallet/validation/TransactionValidation.java
@@ -8,7 +8,7 @@ import com.apps.Ecomwallet.model.UserAccount;
 public class TransactionValidation {
 	
 	public boolean chkEnoughBalance(UserAccount user, double value) {
-		return user.getBalance().getBalanceAmt() <= value;
+		return user.getBalance().getBalanceAmt() >= value;
 	}
 	
 	public boolean chkValidUser(UserAccount user) {
